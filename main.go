544b11f046e10217476f8d8672190d71feeaed2b
Sol <it.is.yurij@gmail.com>
2025-08-07 08:06:43 +1000

    0J/QuNC30LTQvtCy0L7RgNC+0YIh

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
@@ -18,7 +20,7 @@ type content struct {
 	target   string
 	maxSize  int
 	isCached bool
-	cache    map[string]*cache
+	cacheMap map[string]*cache
 	mu       sync.Mutex // * || !* ?
 }
 
@@ -29,8 +31,8 @@ type cache struct {
 
 func newContent() *content {
 	return &content{
-		maxSize: 10 * 1024 * 1024,
-		cache:   map[string]*cache{},
+		maxSize:  10 * 1024 * 1024,
+		cacheMap: map[string]*cache{},
 	}
 }
 
@@ -58,11 +60,23 @@ func fetchHandler(c *content) http.Handler {
 		_, err := downloadContent(c)
 		if err != nil {
 			http.Error(w, "Failed to download file", http.StatusInternalServerError)
+			return
 		}
 	})
 }
 
+func hashFileName(c *content) string {
+	hash := sha256.New()
+	hash.Write([]byte(c.target))
+	hashBytes := hash.Sum(nil)
+	hashString := hex.EncodeToString(hashBytes)
+	// fmt.Println("hashString:", hashString)
+	return hashString
+}
+
 func downloadContent(c *content) (*os.File, error) {
+	// var outFile *os.File
+
 	response, err := http.Get(c.target)
 	if err != nil {
 		log.Println("GET request error:", err)
@@ -70,28 +84,35 @@ func downloadContent(c *content) (*os.File, error) {
 	}
 	defer response.Body.Close()
 
+	fileName := hashFileName(c)
+
+	// if c.isCached {
+	// 	log.Println("File is already in cache")
+	// 	return outFile, nil
+	// }
+
 	contentLength := response.Header.Get("Content-Length")
 	if contentLength == "" {
 		fmt.Println("Failed to get file size.")
 		return nil, err
 	}
-	fmt.Printf("File size: %s byte\n", contentLength)
+	// fmt.Printf("File size: %s byte\n", contentLength)
 
 	intContentLength, _ := strconv.Atoi(contentLength)
 	if intContentLength > c.maxSize {
 		log.Println("File too large. Cannot download it")
-		return nil, nil
+		return nil, err
 	}
 
 	contentType := response.Header.Get("Content-Type")
-	fmt.Println("Content-Type:", contentType)
+	// fmt.Println("Content-Type:", contentType)
 
 	fileType := strings.SplitAfter(contentType, "/")
 	extension := "." + fileType[1]
-	fmt.Println("Extension:", extension)
+	// fmt.Println("Extension:", extension)
 
-	fileName := time.Now().Format("2006-01-02-15-04-05")
-	fmt.Println(fileName)
+	// fileName := time.Now().Format("2006-01-02-15-04-05")
+	// fmt.Println(fileName)
 
 	outFile, err := os.Create(cacheDir + fileName + extension)
 	if err != nil {
@@ -106,7 +127,19 @@ func downloadContent(c *content) (*os.File, error) {
 		return nil, err
 	}
 
+	// fmt.Println("outFile.Name()", outFile.Name())
+	fileStat, _ := outFile.Stat()
+	fmt.Printf("FileName: %v\nSize: %v bytes\nMode: %v\nModTime: %v\nIsDir: %v\nSys: %v\n",
+		fileStat.Name(), fileStat.Size(), fileStat.Mode(), fileStat.ModTime(), fileStat.IsDir(), fileStat.Sys())
+
 	println("Download successful")
 
+	// c.isCached = true
+	// log.Printf("File %s is cached\n", fileStat.Name())
+
 	return outFile, nil
+}
+
+func storeInMap(c *content) {
+
 }
